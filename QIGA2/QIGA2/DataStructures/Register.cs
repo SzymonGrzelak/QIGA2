63a3619bf33ac88b3857a44ff4bc45f687084af0
banan <szy.grzelak@gmail.com>
2019-10-13 12:00:54 +0200

    Initial commit. Add DataStructures: Qubit, Register

---
+++ QIGA2/QIGA2/DataStructures/Register.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QIGA.DataStructures
+{
+    class Register
+    {
+        public List<Qubit> Qubits{ get; set; }
+
+        Register(params Qubit[] list) => this.Qubits = new List<Qubit>(list);
+    }
+}
---
+++ QIGA2/QIGA2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QIGA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
